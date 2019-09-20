78111fdb4e581c67579c77c4e080334ff6b1f6da
Philip Vasilevski <fil13698@gmail.com>
2019-09-20 12:56:11 +0300

    added modal and reworked system edit comments

--- music_site/music/static/music/js/comment_edit.js
+++ music_site/music/static/music/js/comment_edit.js
@@ -1,20 +1,20 @@
 var $csrf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
 
-$(document).ready(function() {
-    $('#edit').on('submit', function(event){
-        event.preventDefault();
-        let comment_id = $('#comment_id').val();
-        $.ajax({
-            type: 'PUT',
-            url: 'http://localhost:8000/api/albums/' + comment_id + '/edit/',
-            headers: {"X-CSRFToken": $csrf_token},
-            data: {
-                'post': $('#input_Text_post').val(),
-                'text': $('#input_Text_text').val(),
-            },
-            success: function(data){
-                document.getElementById("comment_text").innerHTML=data.text;
-            }
-        });
+
+$('#form-edit-save').on('click', function(event){
+    event.preventDefault();
+    $('#modalComments').modal('toggle');
+    let comment_id = $('#comment_id').val();
+    $.ajax({
+        type: 'PUT',
+        url: 'http://localhost:8000/api/albums/' + comment_id + '/edit/',
+        headers: {"X-CSRFToken": $csrf_token},
+        data: {
+            'post': $('#input_Text_post').val(),
+            'text': $('#input_Text_text').val(),
+        },
+        success: function(data){
+            document.getElementById("comment_text").innerHTML=data.text;
+        }
     });
 });
