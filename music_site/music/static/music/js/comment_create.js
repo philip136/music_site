ee46a4e01093b2caf716a3ec6831a47a2b55807f
Philip Vasilevski <fil13698@gmail.com>
2019-10-06 19:09:27 +0300

    window reload after post form

--- music_site/music/static/music/js/comment_create.js
+++ music_site/music/static/music/js/comment_create.js
@@ -1,22 +1,27 @@
 
 $(document).ready(function(){
-    $('#post_form').on('submit', function(event){
-        event.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:8000/api/albums/create/',
-            data: {
-                'post': $('#post').val(),
-                'text': $('#text').val(),
-                'author': $('#author').val(),
-                'album': $('#album').val(),
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-                },
-                success: function(data){
-                    $("div class='comments'/>").append(
-                        $('<div class="post">testtesteasdtea</div>') 
-                    )
-            }
+    $('.btn_create').on('click', function(){
+        $('#post_form').on('submit', function(event){
+            event.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: 'http://localhost:8000/api/albums/create/',
+                data: {
+                    'post': $('#post').val(),
+                    'text': $('#text').val(),
+                    'author': $('#author').val(),
+                    'album': $('#album').val(),
+                    csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+                    },
+                    success: function(data){
+                        // Need create your allert
+                        alert('Comment created');
+                        window.location.reload();
+                    },
+                    eror: function(data){
+                        // Add erors handler for Not Auth
+                    }
+            });
         });
     });
 });
