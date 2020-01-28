37d1575f31d8229a585539885a00c3b8e4bf6f92
Philip Vasilevski <fil13698@gmail.com>
2020-01-28 21:25:00 +0300

    notifications after create event

--- music_site/calendarApp/notifications/consumers.py
+++ music_site/calendarApp/notifications/consumers.py
@@ -5,13 +5,13 @@ class NoseyConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
         await self.accept()
-        await self.channel_layer.group_add("gossip", self.channel_name)
-        print(f"Added {self.channel_name} channel to gossip")
+        await self.channel_layer.group_add("notifications", self.channel_name)
+        print(f"Added {self.channel_name} channel to notifications")
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard("gossip", self.channel_name)
-        print(f"Removed {self.channel_name} channel to gossip")
+        await self.channel_layer.group_discard("notifications", self.channel_name)
+        print(f"Removed {self.channel_name} channel to notifications")
 
-    async def user_gossip(self, event):
+    async def calendar_notifications(self, event):
         await self.send_json(event)
         print(f"Got message {event} at {self.channel_name}")
--- music_site/calendarApp/notifications/signals.py
+++ music_site/calendarApp/notifications/signals.py
@@ -1,5 +1,4 @@
 from calendarApp.models import Calendar
-from django.contrib.auth.models import User
 from datetime import (datetime,
                       timedelta)
 from django.db.models.signals import post_save
@@ -8,11 +7,21 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
 
-@receiver(post_save, sender=User)
-def announce_new_user(sender, instance, created, **kwargs):
+@receiver(post_save, sender=Calendar)
+def announce_new_notification(sender, instance, created, **kwargs):
     if created:
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            "gossip", {"type": "user.gossip",
-                       "event": "New User",
-                       "username": instance.username})
+        current_time = datetime.now()
+        # one or more days
+        if instance.end_event.date() - current_time.date() >= timedelta(days=1):
+            print("success")
+            channel_layer = get_channel_layer()
+            days_left = instance.end_event.date() - current_time.date()
+            async_to_sync(channel_layer.group_send)(
+                "notifications",
+                {
+                 "type": "calendar.notifications",
+                 "event": "New Notification",
+                 "username": instance.user.user.username,
+                 "message": f"{days_left} days left to complete the event {instance.title}",
+                 }
+            )
